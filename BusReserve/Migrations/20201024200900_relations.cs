f570c0176d960ee2ae1fac05ec83cac6c92755f6
nadeesha97 <nadeeshamadhushan9@gmail.com>
2020-10-25 01:40:06 +0530

    Relations added

---
+++ BusReserve/Migrations/20201024200900_relations.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BusReserve.Migrations
+{
+    public partial class relations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BusId",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RouteId",
+                table: "Buses",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_BusId",
+                table: "Users",
+                column: "BusId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Buses_RouteId",
+                table: "Buses",
+                column: "RouteId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Buses_BusRoutes_RouteId",
+                table: "Buses",
+                column: "RouteId",
+                principalTable: "BusRoutes",
+                principalColumn: "RouteId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Buses_BusId",
+                table: "Users",
+                column: "BusId",
+                principalTable: "Buses",
+                principalColumn: "BusId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Buses_BusRoutes_RouteId",
+                table: "Buses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Buses_BusId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_BusId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Buses_RouteId",
+                table: "Buses");
+
+            migrationBuilder.DropColumn(
+                name: "BusId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "RouteId",
+                table: "Buses");
+        }
+    }
+}
--- BusReserve/Models/User.cs
+++ BusReserve/Models/User.cs
@@ -11,5 +11,6 @@ namespace BusReserve.Models
         [Key]
         public string UserId { get; set; }
         public string Password { get; set; }
+        public Bus Bus { get; set; }
     }
 }
